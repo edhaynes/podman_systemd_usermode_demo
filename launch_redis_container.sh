60f8e8561a25380fe7c9260f67b7a170355d35fc
ehaynes <ehaynes@localhost.localdomain>
2020-12-09 10:32:02 -0500

    modified so podman creates container vice running it initially so it doesn't fail at systemd start

--- launch_redis_container.sh
+++ launch_redis_container.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-podman pull redis
-sudo setsebool -P container_manage_cgroup on
-podman run -d --name redis_server -p 6379:6379 redis
+podman create --name redis_server -p 6379:6379 redis
 podman generate systemd redis_server --restart-policy=always -t 5 -f -n
 mkdir -p ~/.config/systemd/user
 cp ./container-redis_server.service ~/.config/systemd/user/redis_server.service
